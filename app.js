4fcf9792fc5b00c7383fb630b83a9ede0b7ec562
Martsynyuk <terr83@gmail.com>
2017-06-12 16:57:30 +0300

    1

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 'use strict';
 
-var express = require('express');
-var app = express();
+var express     = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var app         = express();
+
+app.set('view engine', 'jade');
+app.set('views', 'views');
 
 app.get('/', function(request, response) {
-    response.send('test')
+    /*MongoClient.connect('mongodb://localhost:27015/dbName', function(err, db){
+        console.log(db);
+    });*/
+    //response.send('test');
+    response.render('index',
+        {
+            title: 'Hey',
+            message: 'Hello there lol!',
+            text: {'s':'asdasd', 'v':'24234'}
+        });
 });
 
 app.listen(8080, function () {
