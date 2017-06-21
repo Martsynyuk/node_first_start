bf2e532b0eed8cb43f0280de4a85ab772b1f5c0f
Martsynyuk <terr83@gmail.com>
2017-06-21 13:00:12 +0300

    1

--- app.js
+++ app.js
@@ -2,21 +2,30 @@
 
 var express     = require('express'),
     app         = express(),
-    db          = require('./core/dbConnect');
+    db          = require('./core/dbConnect'),
+    bodyParser  = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'jade');
 app.set('views', 'views');
 
-app.get('/', function(request, response) {
-    response.render('index',
+app.get('/', function(req, res) {
+    res.render('index',
         {
+            content: 'Home page',
             title: 'Hey',
             message: 'Hello there lol!',
             text: {'s':'asdasd', 'v':'24234'}
         });
 });
 
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!');
+app.get('/login', function (req, res) {
+    res.render('login', {
+        content: 'Sign In',
+        title: 'login'
+    });
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
 });
---
+++ public/js/main.js
@@ -0,0 +1,3 @@
+/**
+ * Created by antonsoft on 21.06.2017.
+ */
