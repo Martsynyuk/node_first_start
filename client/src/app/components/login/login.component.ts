eb3de57984ba37352f9402ecb435e2cc4dcbc461
Martsynyuk <terr83@gmail.com>
2017-07-27 11:19:44 +0300

    fix selector

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-root',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
