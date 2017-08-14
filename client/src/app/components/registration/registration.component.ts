3e53106243d4cf62eab88fd35416dedf1815c744
Kolya <nick97it@gmail.com>
2017-08-14 13:12:07 +0300

    custom validation

--- client/src/app/components/registration/registration.component.ts
+++ client/src/app/components/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, ValidationErrors} from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 import { User } from '../../models/user';
 import { UserValidate } from '../../validations/user_validate';
 
@@ -11,19 +11,46 @@ import { UserValidate } from '../../validations/user_validate';
 
 export class RegistrationComponent implements OnInit {
 
-  constructor() {
-
-  }
-
-  user      = new User('Terr', '', '', '');
+  Errors = {
+    'user_name': {
+      'required': 'Name is required',
+      'minlength': 'Name must be more 6 charset',
+      'manlength': 'Name must be less 6 charset'
+    },
+    'user_email': {
+      'required': 'Email is required',
+      'email': 'Email is not valid'
+    },
+    'user_password': {
+      'required': 'Password is required'
+    },
+    'user_confirm_password': {
+      'required': 'Confirm password is required'
+    }
+  };
+  user      = new User('', '', '', '');
   regForm   = new FormGroup(new UserValidate().regValid);
   submitted = false;
 
+  valid = [];
+
+  constructor() {
+  }
+
   addUser() {
-    this.submitted = true;
-    console.log(this.user);
+    this.valid = [];
+
+    for (var formControlName in this.regForm.controls) {
+      for (var validator in this.Errors[`${formControlName}`]) {
+        if (this.regForm.controls[`${formControlName}`].hasError(`${validator}`)) {
+          this.valid[`${formControlName}`] = this.Errors[`${formControlName}`][`${validator}`];
+        }
+      }
+    }
+    console.log(this.valid);
   }
 
   ngOnInit() {
+
   }
 }
